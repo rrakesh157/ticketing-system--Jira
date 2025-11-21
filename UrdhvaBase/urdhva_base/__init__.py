9518ecc7a88acb8b96774f083e3c893a1f1075ec
Rakesh <rachakondarakesh157@gmail.com>
2025-11-21 13:33:14 +0530

    updated

--- UrdhvaBase/urdhva_base/__init__.py
+++
@@ -1,8 +0,0 @@
-from .context import context as ctx
-from .settings import settings
-from .logger import Logger
-from .types import Secret
-from .entity import Entity
-from .postgresmodel import BasePostgresModel
-from .postgresmodel import PostgresModel
-from .queryparams import QueryParams
